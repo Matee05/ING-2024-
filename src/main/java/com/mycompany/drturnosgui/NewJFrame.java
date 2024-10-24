523cf989a392c6cf147a10940a21f53ba8122e4b
camiit <camilaangelinatenorio@gmail.com>
2024-10-24 11:47:57 -0300

    Commit 4 (btnPacientes)

--- src/main/java/com/mycompany/drturnosgui/NewJFrame.java
+++ src/main/java/com/mycompany/drturnosgui/NewJFrame.java
@@ -1,6 +1,16 @@
 
 package com.mycompany.drturnosgui;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class NewJFrame extends javax.swing.JFrame {
+    
+    //Sets para guardas turnos, clientes y obras sociales
+    private Set<String> turnosSet = new HashSet<>();
+    public static Set<Cliente> clientes = new HashSet<>();
+    public static Set<ObraSocial> obrasSociales = new HashSet<>();
+    
     public NewJFrame() {
         initComponents();
     }
@@ -12,10 +22,10 @@ public class NewJFrame extends javax.swing.JFrame {
         jScrollPane2 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         jPanel3 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
+        btnPacientes = new javax.swing.JButton();
+        btnObrasSociales = new javax.swing.JButton();
+        btnAgregarTurno = new javax.swing.JButton();
+        btnEliminarTurno = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -49,18 +59,23 @@ public class NewJFrame extends javax.swing.JFrame {
 
         PanelPrincipal.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 0, 550, 570));
 
-        jButton1.setText("Pacientes");
-
-        jButton2.setText("Obras sociales");
-
-        jButton3.setText("Agregar/Modificar");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        btnPacientes.setText("Pacientes");
+        btnPacientes.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                btnPacientesActionPerformed(evt);
             }
         });
 
-        jButton4.setText("Eliminar");
+        btnObrasSociales.setText("Obras sociales");
+
+        btnAgregarTurno.setText("Agregar/Modificar");
+        btnAgregarTurno.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAgregarTurnoActionPerformed(evt);
+            }
+        });
+
+        btnEliminarTurno.setText("Eliminar");
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
@@ -69,23 +84,23 @@ public class NewJFrame extends javax.swing.JFrame {
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE)
-                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(btnObrasSociales, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnAgregarTurno, javax.swing.GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE)
+                    .addComponent(btnEliminarTurno, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnPacientes, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addGap(5, 5, 5)
-                .addComponent(jButton1)
+                .addComponent(btnPacientes)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton2)
+                .addComponent(btnObrasSociales)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton3)
+                .addComponent(btnAgregarTurno)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton4)
+                .addComponent(btnEliminarTurno)
                 .addContainerGap())
         );
 
@@ -96,24 +111,32 @@ public class NewJFrame extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+    private void btnAgregarTurnoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarTurnoActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
+    }//GEN-LAST:event_btnAgregarTurnoActionPerformed
+
+    private void btnPacientesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPacientesActionPerformed
+        ClientesGUI clientesGUI = new ClientesGUI(clientes, obrasSociales);
+        clientesGUI.setVisible(true);
+        clientesGUI.setLocationRelativeTo(null);
+    }//GEN-LAST:event_btnPacientesActionPerformed
 
     public static void main(String args[]) {
 
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame().setVisible(true);   
+                NewJFrame turnosGUI = new NewJFrame();
+                turnosGUI.setVisible(true);
+                turnosGUI.setLocationRelativeTo(null);
             }
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel PanelPrincipal;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
+    private javax.swing.JButton btnAgregarTurno;
+    private javax.swing.JButton btnEliminarTurno;
+    private javax.swing.JButton btnObrasSociales;
+    private javax.swing.JButton btnPacientes;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
